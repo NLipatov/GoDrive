bb05c79d77a86dae59ca093db293a48618e12090
Nikita <6stringsohei@gmail.com>
2024-08-28 20:24:35 +0300

    AutoDeletionChecksMinutes prop added to config

--- src/Infrastructure/RepositoryConfiguration.go
+++ src/Infrastructure/RepositoryConfiguration.go
@@ -7,8 +7,9 @@ import (
 )
 
 type PersistenceConfig struct {
-	EnableAutoDeletion          bool `json:"EnableAutoDeletion"`
-	AutoDeletionIntervalMinutes int  `json:"AutoDeletionIntervalMinutes"`
+	EnableAutoDeletion               bool `json:"EnableAutoDeletion"`
+	AutoDeletionCheckIntervalMinutes int  `json:"AutoDeletionCheckIntervalMinutes"`
+	AutoDeletionIntervalMinutes      int  `json:"AutoDeletionIntervalMinutes"`
 }
 
 type AppSettings struct {
@@ -16,18 +17,29 @@ type AppSettings struct {
 	Persistence      PersistenceConfig `json:"Persistence"`
 }
 
-func GetConfiguration() (AppSettings, error) {
+func GetConfiguration() AppSettings {
 	path, err := os.Getwd()
 	if err != nil {
-		return AppSettings{}, err
+		return defaultConfiguration()
 	}
 
 	bytes, err := os.ReadFile(filepath.Join(path, "config.json"))
 	if err != nil {
-		return AppSettings{}, err
+		return defaultConfiguration()
 	}
 
 	var appSettings AppSettings
 	err = json.Unmarshal(bytes, &appSettings)
-	return appSettings, nil
+	return appSettings
+}
+
+func defaultConfiguration() AppSettings {
+	return AppSettings{
+		StorageDirectory: "storage",
+		Persistence: PersistenceConfig{
+			EnableAutoDeletion:               true,
+			AutoDeletionCheckIntervalMinutes: 30,
+			AutoDeletionIntervalMinutes:      180,
+		},
+	}
 }
