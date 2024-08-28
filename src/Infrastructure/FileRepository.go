de997565c6bbec3142a73f1e70a7c0bde3ead328
Nikita <6stringsohei@gmail.com>
2024-08-28 18:09:42 +0300

    GET and DELETE implemented

--- src/Infrastructure/FileRepository.go
+++ src/Infrastructure/FileRepository.go
@@ -7,39 +7,51 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 type FileRepository struct{}
 
 func (s *FileRepository) Get(id uuid.UUID) (Domain.File, error) {
-	panic("implement me")
+	storageDirectoryPath, err := getStorageDirectoryPath()
+	if err != nil {
+		return Domain.File{}, err
+	}
+
+	file, err := os.Open(filepath.Join(storageDirectoryPath, id.String()))
+	if err != nil {
+		return Domain.File{}, err
+	}
+
+	fileContent, err := io.ReadAll(file)
+	if err != nil {
+		return Domain.File{}, err
+	}
+
+	return Domain.File{
+		Id:        id,
+		Data:      fileContent,
+		ExpiresAt: time.Time{},
+	}, nil
 }
 
 func (s *FileRepository) Save(file Domain.File) (uuid.UUID, error) {
-	currentDirectory, err := os.Getwd()
+	storageDirectoryPath, err := getStorageDirectoryPath()
 	if err != nil {
 		return uuid.Nil, err
-	}
-
-	storageDirectory := filepath.Join(currentDirectory, "storage")
-	if _, err = os.Stat(storageDirectory); os.IsNotExist(err) {
-		err = os.Mkdir(storageDirectory, 0755)
-		if err != nil {
-			return uuid.Nil, err
-		}
 	}
 
 	for {
 		file.Id = uuid.New()
 
-		filePath := filepath.Join(storageDirectory, file.Id.String())
+		filePath := filepath.Join(storageDirectoryPath, file.Id.String())
 
 		if _, err = os.Stat(filePath); os.IsNotExist(err) {
 			break
 		}
 	}
 
-	writeToFile, err := os.Create(filepath.Join(storageDirectory, file.Id.String()))
+	writeToFile, err := os.Create(filepath.Join(storageDirectoryPath, file.Id.String()))
 	if err != nil {
 		return uuid.Nil, err
 	}
@@ -54,5 +66,33 @@ func (s *FileRepository) Save(file Domain.File) (uuid.UUID, error) {
 }
 
 func (s *FileRepository) Delete(id uuid.UUID) (uuid.UUID, error) {
-	panic("implement me")
+	storageDirectoryPath, err := getStorageDirectoryPath()
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	filePath := filepath.Join(storageDirectoryPath, id.String())
+	err = os.Remove(filePath)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	return uuid.Nil, nil
+}
+
+func getStorageDirectoryPath() (string, error) {
+	currentDirectory, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+
+	storageDirectory := filepath.Join(currentDirectory, "storage")
+	if _, err = os.Stat(storageDirectory); os.IsNotExist(err) {
+		err = os.Mkdir(storageDirectory, 0755)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	return storageDirectory, nil
 }
